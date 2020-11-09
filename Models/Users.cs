571dd79df2b45c5512b033ffd630b3bc4c3dd3f9
Liaihua <ilyavovchenko431@gmail.com>
2020-11-09 15:27:23 +1000

    Добавьте файлы проекта.

---
+++ Models/Users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1141_WebApp.Models
+{
+    public partial class Users
+    {
+        public int Id { get; set; }
+        public string Fname { get; set; }
+        public string Sname { get; set; }
+        public int IdDepartment { get; set; }
+
+        public virtual Departments IdDepartmentNavigation { get; set; }
+    }
+}
